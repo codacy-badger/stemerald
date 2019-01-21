53b480c9ad6cb23e98afeea8f315f64225012235
Mahdi Aali <mahdi13.1373@gmail.com>
2019-01-21 19:04:51 +0300

    old market controller has been removed

--- stemerald/controllers/trading.py
+++ stemerald/controllers/trading.py
@@ -1,72 +1,12 @@
 from typing import Union
 
 from nanohttp import json, RestController
-from nanohttp.exceptions import HttpNotFound
 
 from restfulpy.authorization import authorize
-from restfulpy.controllers import ModelRestController
-from restfulpy.orm import commit
 from restfulpy.validation import validate_form, prevent_form
-
-from stemerald.models.trading import Market
 
 BidId = Union[int, str]
 TradeId = Union[int, str]
-
-
-class MarketController(ModelRestController):
-    __model__ = Market
-
-    @json
-    @validate_form(
-        whitelist=['sort', 'id', 'baseCurrencyId', 'quoteCurrencyId'],
-        pattern={'sort': r'^(-)?(id|baseCurrencyId|quoteCurrencyId)$'}
-    )
-    @__model__.expose
-    def get(self, market_id: int = None):
-        if market_id is None:
-            return Market.query
-
-        else:
-            market = Market.query.filter(Market.id == market_id).one_or_none()
-            if market is None:
-                raise HttpNotFound('Market not found', 'market-not-found')
-
-            return market.to_dict()
-
-    @json
-    @authorize('admin')
-    @validate_form(
-        whitelist=['buyAmountMin', 'buyAmountMax', 'buyStaticCommission', 'buyStaticCommission',
-                   'buyPermilleCommission', 'buyMaxCommission', 'sellAmountMin', 'sellAmountMax',
-                   'sellStaticCommission', 'sellPermilleCommission', 'sellMaxCommission']
-    )
-    @__model__.expose
-    @commit
-    def edit(self, market_id: int):
-        market = Market.query.filter(Market.id == market_id).one_or_none()
-
-        if market is None:
-            raise HttpNotFound()
-
-        market.update_from_request()
-        return market
-
-    @json
-    @validate_form(
-        whitelist=['take', 'skip'],
-        types={'take': int, 'skip': int}
-    )
-    def present(self, market_id: int, inner_resource: str):
-        # TODO
-        pass
-
-    @json
-    @authorize('admin', 'client')
-    @validate_form(exact=['type', 'price', 'amount'], types={'price': int, 'amount': int, 'marketId': int})
-    def calculate(self, market_id: int):
-        # TODO
-        pass
 
 
 class OrderController(RestController):
