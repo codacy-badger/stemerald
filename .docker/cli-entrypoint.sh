d6081520c48a758135d209268ed8a9e149f974b4
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-10 02:06:22 +0300

    Change docker files stracture

--- .docker/cli-entrypoint.sh
+++ .docker/cli-entrypoint.sh
@@ -6,4 +6,8 @@ wait-for-it pgdb:5432 -- echo "Postgres is ready"
 echo "Waiting for redis server to be ready ..."
 wait-for-it redisdb:6379 -- echo "Redis is ready"
 
+wait 60
+
 envsubst < /etc/stemerald/config.template.yml > ${STEMERALD_CONFIG_FILE}
+
+tail -f /dev/null
--- .docker/worker-entrypoint.sh
+++ .docker/worker-entrypoint.sh
@@ -6,6 +6,8 @@ wait-for-it pgdb:5432 -- echo "Postgres is ready"
 echo "Waiting for redis server to be ready ..."
 wait-for-it redisdb:6379 -- echo "Redis is ready"
 
+wait 60
+
 envsubst < /etc/stemerald/config.template.yml > ${STEMERALD_CONFIG_FILE}
 
 stemerald worker start -c ${STEMERALD_CONFIG_FILE}
