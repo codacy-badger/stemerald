9fff7f4b0f915bbca08f282cafe89e3bd461ec5f
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-09 22:00:56 +0300

    Refactore travis config

--- docker/worker/entrypoint.sh
+++ docker/worker/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-
-
 envsubst < /etc/stemerald/config.template > ${STEMERALD_CONFIG_FILE}
-
-while !</dev/tcp/db/5432; do sleep 1; done;
 
 stemerald worker start -c ${STEMERALD_CONFIG_FILE}
