464f306d45f34df5ab344fc6b3068e90f5dcf569
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-10 00:44:51 +0300

    Change docker files stracture

---
+++ .docker/wsgi-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Waiting for postgres server to be ready ..."
+wait-for-it pgdb:5432 -- echo "Postgres is ready"
+
+echo "Waiting for redis server to be ready ..."
+wait-for-it redisdb:6379 -- echo "Redis is ready"
+
+envsubst < /etc/stemerald/config.template.yml > ${STEMERALD_CONFIG_FILE}
+
+stemerald -c ${STEMERALD_CONFIG_FILE} admin create-db --basedata
+stemerald -c ${STEMERALD_CONFIG_FILE} migrate upgrade head
+
+gunicorn -w 4 --bind 0.0.0.0:8080 'wsgi:app'
--- docker/worker/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-envsubst < /etc/stemerald/config.template > ${STEMERALD_CONFIG_FILE}
-
-stemerald worker start -c ${STEMERALD_CONFIG_FILE}
