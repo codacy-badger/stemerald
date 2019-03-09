aa7431c9927e1d3df2e01226a877ef6b28a58c6a
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-09 10:03:45 +0300

    Fix docker postgres config

--- docker/postgresql/init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER dev;
-    CREATE DATABASE stemerald_db;
-    GRANT ALL PRIVILEGES ON DATABASE stemerald_db TO dev;
-    ALTER USER dev WITH PASSWORD 'dev-secret    ';
-EOSQL
