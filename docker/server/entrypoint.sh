2472624d138b271fa285b6332c393ec44f276b97
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-09 13:47:20 +0300

    Fixing dockerfile

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+envsubst < /etc/stemerald/config.template > ${STEMERALD_CONFIG_FILE}
+stemerald -c ${STEMERALD_CONFIG_FILE} admin create-db --base-data
+stemerald -c ${STEMERALD_CONFIG_FILE} migrate upgrade head
+
 gunicorn -w 4 --bind 0.0.0.0:8080 'wsgi:app'
--- docker/worker/entrypoint.sh
+++ docker/worker/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+envsubst < /etc/stemerald/config.template > ${STEMERALD_CONFIG_FILE}
+
 stemerald worker start -c ${STEMERALD_CONFIG_FILE}
